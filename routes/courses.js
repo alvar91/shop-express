8653c30d959ec7c01a91aa881ba4f0bdcfe2cb7b
Aleksey <aleksey91scorp@bk.ru>
2020-05-28 13:10:37 +0300

    dynamic layout for course added

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,23 @@
-const {Router} = require('express')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
-router.get('/', async (req, res) => {
-  const courses = await Course.getAll()
-  res.render('courses', {
-    title: 'Курсы',
+router.get("/", async (req, res) => {
+  const courses = await Course.getAll();
+  res.render("courses", {
+    title: "Курсы",
     isCourses: true,
-    courses
-  })
-})
+    courses,
+  });
+});
 
-module.exports = router
+router.get("/:id", async (req, res) => {
+  const course = await Course.getById(req.params.id);
+  res.render("course", {
+    layout: "empty",
+    title: `Курс ${course.title}`,
+    course,
+  });
+});
+
+module.exports = router;
